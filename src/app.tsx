f6fd43fd4433dfdab37e0ddace4a8ecf13d1fb26
Martin Lichstam <martin.lichstam@gmail.com>
2022-06-02 12:58:24 +0200

    Add Spacings and font sizes to stiches config

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,8 @@ import { styled } from './stitches.config'
 const Button = styled('button', {
   backgroundColor: 'gainsboro',
   borderRadius: '9999px',
-  fontSize: '13px',
-  padding: '10px 15px',
+  fontSize: '$xl',
+  padding: '$md $xl',
   '&:hover': {
     backgroundColor: 'lightgray',
   },
