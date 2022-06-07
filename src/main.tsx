a736d5a891f3e295ca36c2f184ef349b1f81ba06
Martin Lichstam <martin.lichstam@gmail.com>
2022-06-07 14:32:12 +0200

    Fix font import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './containers/main'
+import '../fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
