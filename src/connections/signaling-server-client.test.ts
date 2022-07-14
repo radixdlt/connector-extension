36109bf653927834382084f936b67dbb54c3012c
Alex Stelea <alex.s@rdx.works>
2022-07-14 10:11:49 +0200

    move helper into test

--- src/connections/signaling-server-client.test.ts
+++ src/connections/signaling-server-client.test.ts
@@ -2,11 +2,17 @@ import WSS from 'jest-websocket-mock'
 import { signalingServerClient } from './signaling-server-client'
 import { Status, wsStatusSubject } from './subjects'
 import { subscribeSpyTo } from '@hirez_io/observer-spy'
+import { filter, firstValueFrom } from 'rxjs'
 
 const url = 'ws://localhost:1234'
 let wss: WSS
 let client = signalingServerClient(url)
 let wsStatusSpy: ReturnType<typeof subscribeSpyTo<Status>>
+
+const waitUntilConnected = async () =>
+firstValueFrom(
+  wsStatusSubject.pipe(filter((status) => status === 'connected'))
+)
 
 describe('Signaling server client', () => {
   beforeEach(async () => {
@@ -20,7 +26,7 @@ describe('Signaling server client', () => {
 
   it('should successfully connect and emit status', async () => {
     client.connect()
-    await client.waitUntilConnected()
+    await waitUntilConnected()
     expect(wsStatusSpy.getValues()).toEqual(['connecting','connected'])
   })
 })
