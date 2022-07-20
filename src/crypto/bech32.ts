0fc6dbd663b69f8d2bbcf4103c2a4e09216ac250
Alex Stelea <alex.s@rdx.works>
2022-07-20 10:38:54 +0200

    add bech32 encode

--- src/crypto/bech32.ts
+++ src/crypto/bech32.ts
@@ -1,9 +1,18 @@
-import { ok, Result } from 'neverthrow'
+import { err, ok } from 'neverthrow'
 import { bech32 } from 'bech32'
 
-export const convertBufferToBech32 = (
-  buffer: Buffer
-): Result<Buffer, Error> => {
+export const convertBufferToBech32 = (buffer: Buffer) => {
   const bech32Data = bech32.toWords(buffer)
   return ok(Buffer.from(bech32Data))
+}
+
+export const bech32Encode = (buffer: Buffer) => {
+  if (buffer.length > 90) {
+    return err(new Error('Buffer exceeds length limit'))
+  }
+  const bech32String = bech32
+    .encode('', buffer)
+    // drop bech32 delimiter/separator and checksum
+    .slice(1, -6)
+  return ok(bech32String)
 }
