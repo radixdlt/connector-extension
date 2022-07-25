7ddbf35c4223e7cd26d52abec04e77445c286672
Alex Stelea <alex.s@rdx.works>
2022-07-25 15:26:35 +0200

    add sealbox function

---
+++ src/crypto/sealbox.ts
@@ -0,0 +1,47 @@
+import { combine, Result } from 'neverthrow'
+import { readBuffer } from 'utils/buffer-reader'
+
+type SealedBoxProps = {
+  ciphertext: Buffer
+  iv: Buffer
+  authTag: Buffer
+  combined: Buffer
+  ciphertextAndAuthTag: Buffer
+}
+
+const combineSealboxToBuffer = ({
+  iv,
+  ciphertext,
+  authTag,
+}: Pick<SealedBoxProps, 'iv' | 'ciphertext' | 'authTag'>): Buffer =>
+  Buffer.concat([iv, ciphertext, authTag])
+
+const combineCiphertextAndAuthtag = ({
+  ciphertext,
+  authTag,
+}: Pick<SealedBoxProps, 'ciphertext' | 'authTag'>): Buffer =>
+  Buffer.concat([ciphertext, authTag])
+
+export const transformBufferToSealbox = (
+  buffer: Buffer
+): Result<SealedBoxProps, Error> => {
+  const readNextBuffer = readBuffer(buffer)
+
+  const nonceLength = 12
+  const authTagLength = 16
+
+  return combine([
+    readNextBuffer(nonceLength),
+    readNextBuffer(buffer.length - nonceLength - authTagLength),
+    readNextBuffer(authTagLength),
+  ]).map(([iv, ciphertext, authTag]: Buffer[]) => ({
+    iv,
+    ciphertext,
+    authTag,
+    combined: combineSealboxToBuffer({ iv, ciphertext, authTag }),
+    ciphertextAndAuthTag: combineCiphertextAndAuthtag({
+      ciphertext,
+      authTag,
+    }),
+  }))
+}
