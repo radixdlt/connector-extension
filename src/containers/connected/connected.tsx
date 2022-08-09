b850de435ac31ad47f09d97b84fc0e52f5d92976
Alex Stelea <alex.s@rdx.works>
2022-08-09 10:04:48 +0200

    add message with different payload sizes

--- src/containers/connected/connected.tsx
+++ src/containers/connected/connected.tsx
@@ -3,6 +3,14 @@ import { useWebRtcIncomingMessage } from 'hooks/use-rtc-incoming-message'
 import { useWebRtcSendMessage } from 'hooks/use-rtc-send-message'
 import { useEffect, useState } from 'react'
 
+const oneMB = new Array(1000)
+  .fill(null)
+  .map(
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu odio consectetur, varius lorem quis, finibus enim. Aliquam erat volutpat. Vivamus posuere sit amet justo ut vulputate. Nam ultrices nec tortor at pulvinar. Nunc et nibh purus. Donec vehicula venenatis risus eu sollicitudin. Sed posuere eu odio ac semper. Sed vitae est id dui blandit aliquet. Sed dapibus mi dui, ut rhoncus dolor aliquet tempus. Nam fermentum justo a arcu egestas, id laoreet urna condimentum. Nunc auctor elit sed arcu lobortis, a tincidunt libero mollis. Etiam hendrerit eu risus eget porttitor. Donec vitae neque vehicula, cursus magna eget, mollis metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum vel facilisis diam. Sed non tortor ultricies, viverra mauris tempor, cursus justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum vel facilisis diam. Sed non tortor ultricies, viverra mauris tempor, cursu. `
+  )
+  .join('')
+
 export const Connected = () => {
   const [messages, setMessages] = useState<string[]>([])
   const [outgoingMessage, setOutgoingMessage] = useState<string>('')
@@ -31,7 +39,7 @@ export const Connected = () => {
         }}
       >
         <Box
-          css={{ border: '1px solid black', height: '200px' }}
+          css={{ border: '1px solid black', height: '200px', overflow: 'auto' }}
           border
           p="small"
         >
@@ -44,6 +52,44 @@ export const Connected = () => {
           onChange={(event) => setOutgoingMessage(event.target.value)}
         />
         <button type="submit">Send message</button>
+        <Box flex="col">
+          <button
+            style={{ marginTop: 10 }}
+            type="button"
+            onClick={() => {
+              sendMessage(oneMB.slice(0, oneMB.length / 10))
+            }}
+          >
+            Send 0.1MB message
+          </button>
+          <button
+            style={{ marginTop: 10 }}
+            type="button"
+            onClick={() => {
+              sendMessage(oneMB.slice(0, oneMB.length / 5))
+            }}
+          >
+            Send 0.25MB message
+          </button>
+          <button
+            style={{ marginTop: 10 }}
+            type="button"
+            onClick={() => {
+              sendMessage(oneMB.slice(0, oneMB.length / 2))
+            }}
+          >
+            Send 0.5MB message
+          </button>
+          <button
+            style={{ marginTop: 10 }}
+            type="button"
+            onClick={() => {
+              sendMessage(oneMB)
+            }}
+          >
+            Send 1MB message
+          </button>
+        </Box>
       </form>
     </Box>
   )
