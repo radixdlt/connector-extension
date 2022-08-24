54006f0ea4d04487b7a8fb32020acd464c01bf15
Alex Stelea <alex.s@rdx.works>
2022-08-24 10:08:07 +0200

    add support for running tests in docker

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ WORKDIR /home/node/app
 COPY package.json ./
 COPY yarn.lock ./
 
+RUN yarn add wrtc
+
 COPY . .
 
 CMD yarn test
--- src/setup-tests.js
+++ src/setup-tests.js
@@ -2,10 +2,12 @@
 require('dotenv').config()
 import { Crypto } from '@peculiar/webcrypto'
 import { chrome } from 'jest-chrome'
-import webRTC from '@koush/wrtc'
 
 global.crypto = new Crypto()
 global.chrome = chrome
-global.RTCPeerConnection = webRTC.RTCPeerConnection
-global.RTCIceCandidate = webRTC.RTCIceCandidate
-global.RTCSessionDescription = webRTC.RTCSessionDescription
+
+import(process.env.CI ? 'wrtc' : '@koush/wrtc').then((webRTC) => {
+  global.RTCPeerConnection = webRTC.RTCPeerConnection
+  global.RTCIceCandidate = webRTC.RTCIceCandidate
+  global.RTCSessionDescription = webRTC.RTCSessionDescription
+})
