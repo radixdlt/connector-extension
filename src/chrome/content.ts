e4e597bb5366fdfc6253f651912b64ae567d9695
Alex Stelea <alex.s@rdx.works>
2022-09-09 11:54:21 +0200

    send messages from wallet to sdk

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -1,4 +1,5 @@
 import { Bootstrap } from 'bootstrap/bootstrap'
+import { parseJSON } from 'utils'
 
 const application = Bootstrap({
   logLevel: 'debug',
@@ -11,4 +12,14 @@ window.addEventListener('radix#chromeExtension#send', (event) => {
   const { detail: message } = event as CustomEvent<any>
   application.webRtc.subjects.rtcAddMessageToQueue.next(message)
   chrome.runtime.sendMessage({})
+})
+
+application.webRtc.subjects.rtcIncomingMessageSubject.subscribe((raw) => {
+  parseJSON(raw).map((message) =>
+    window.dispatchEvent(
+      new CustomEvent('radix#chromeExtension#receive', {
+        detail: message,
+      })
+    )
+  )
 })
