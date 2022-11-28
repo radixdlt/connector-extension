926dd536f00765008292e0cf50096836e8a0b79f
Alex Stelea <alex.s@rdx.works>
2022-11-28 10:56:46 +0100

    build: update permissions
    [ABW-625] Use the minimum set of required extension permissions.

--- src/chrome/helpers/get-active-tab.ts
+++
@@ -1,7 +0,0 @@
-import { ResultAsync } from 'neverthrow'
-
-export const getActiveTab = () =>
-  ResultAsync.fromPromise(
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }),
-    (error) => error as Error
-  ).map((tabs) => tabs[0])
