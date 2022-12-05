8aa7507997b831392eefa4b5c7114a5814e4d9a2
Alex Stelea <alex.s@rdx.works>
2022-12-05 10:29:36 +0100

    fix: restart connection when forget wallet clicked
    [ABW-691] Should trigger a webRTC restart on a active connection when password is changed

--- src/connector/observables/rtc-restart.ts
+++ src/connector/observables/rtc-restart.ts
@@ -1,12 +1,23 @@
 import log from 'loglevel'
-import { withLatestFrom, tap } from 'rxjs'
+import { withLatestFrom, tap, merge, filter } from 'rxjs'
 import { ConnectorSubscriptionsInput } from 'connector/_types'
 
 export const rtcRestart = (input: ConnectorSubscriptionsInput) => {
   const signalingSubjects = input.signalingServerClient.subjects
   const webRtcSubjects = input.webRtcClient.subjects
 
-  return webRtcSubjects.rtcRestartSubject.pipe(
+  const forceRestart$ = webRtcSubjects.rtcRestartSubject
+
+  const restartActiveConnectionWhenConnectionPasswordChanged$ =
+    input.storageClient.subjects.onPasswordChange.pipe(
+      withLatestFrom(webRtcSubjects.rtcStatusSubject),
+      filter(([_, status]) => status !== 'disconnected')
+    )
+
+  return merge(
+    forceRestart$,
+    restartActiveConnectionWhenConnectionPasswordChanged$
+  ).pipe(
     withLatestFrom(signalingSubjects.wsSourceSubject),
     tap(([, source]) => {
       log.debug(`🕸🔄 [${source}] restarting webRTC...`)
