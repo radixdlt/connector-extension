acb33005121664e891de158a92463a7e7b35392a
Alex Stelea <alex.s@rdx.works>
2022-12-05 10:40:57 +0100

    chore: remove mixpanel analytics
    [ABW-623]

--- src/connector/signaling/observables/ws-connection-secrets.ts
+++ src/connector/signaling/observables/ws-connection-secrets.ts
@@ -1,7 +1,6 @@
 import { config } from 'config'
 import { deriveSecretsFromConnectionPassword } from '../secrets'
 import { secureRandom } from 'crypto/secure-random'
-import { setConnectionId } from 'mixpanel'
 import { errAsync } from 'neverthrow'
 import { tap, switchMap, share } from 'rxjs'
 import { SignalingSubjectsType } from 'connector/signaling/subjects'
@@ -31,10 +30,5 @@ export const wsConnectionPassword = (
         : errAsync(Error('missing connection password'))
     ),
     share(),
-    tap((result) => {
-      if (result.isOk())
-        setConnectionId(result.value.connectionId.toString('hex'))
-
-      return subjects.wsConnectionSecretsSubject.next(result)
-    })
+    tap((result) => subjects.wsConnectionSecretsSubject.next(result))
   )
