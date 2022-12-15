a3c5829c3a74d5ec9b8925633b6d4ea45fc012ea
Alex Stelea <alex.s@rdx.works>
2022-12-15 11:34:37 +0100

    build: silence logs on beta build

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -1,18 +1,27 @@
+import { config } from 'config'
 import { ChromeConnectorClient } from './chrome-connector-client'
 import { ChromeDAppClient, messageLifeCycleEvent } from './chrome-dapp-client'
 import { decorateMessage } from './helpers/decorate-message'
 
-const connectorClient = ChromeConnectorClient('debug')
+const connectorClient = ChromeConnectorClient(config.logLevel)
 const chromeDAppClient = ChromeDAppClient()
 
 chromeDAppClient.messageListener((message) => {
-  decorateMessage(message)
-    .map(connectorClient.getConnector().sendMessage)
-    .map(chrome.runtime.sendMessage)
-    .andThen(() =>
-      chromeDAppClient.sendMessageEvent(
-        message.requestId,
-        messageLifeCycleEvent.receivedByExtension
-      )
+  if (message.type === 'debugMode') {
+    const loglevel = message.value ? 'DEBUG' : 'INFO'
+    console.log(
+      `🛠 Setting loglevel to: ${loglevel}, reload the window for effects to take place`
     )
+    chrome.storage.local.set({ loglevel })
+  } else {
+    decorateMessage(message)
+      .map(connectorClient.getConnector().sendMessage)
+      .map(chrome.runtime.sendMessage)
+      .andThen(() =>
+        chromeDAppClient.sendMessageEvent(
+          message.requestId,
+          messageLifeCycleEvent.receivedByExtension
+        )
+      )
+  }
 })
