b9d989b5442af2a3ac3e45e7cf77d796b0edff91
Dawid Sowa <dawid.sowa@rdx.works>
2023-04-26 12:35:07 +0200

    fix: derivation path encoding

--- src/ledger/ledger-wrapper.ts
+++ src/ledger/ledger-wrapper.ts
@@ -64,8 +64,10 @@ export const encodeDerivationPath = (derivationPath: string) => {
   const length = `00${(path.length & 0xff).toString(16)}`.slice(-2)
 
   const parts = path
-    .map((value) => 0x80000000 + parseInt(value, 10))
-    .map((value) => value.toString(16))
+    .map(
+      (value) => (value.endsWith("'") ? 0x80000000 : 0) + parseInt(value, 10)
+    )
+    .map((value) => value.toString(16).padStart(8, '0'))
     .join('')
 
   const data = `${length}${parts}`
