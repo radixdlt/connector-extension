b79fcf5e66b628eb5780072e1c40c49ddef8782c
Dawid Sowa <dawid.sowa@rdx.works>
2023-05-23 14:54:18 +0200

    feat: add signature verify for tx & auth signing

--- src/crypto/blake2b.spec.ts
+++ src/crypto/blake2b.spec.ts
@@ -1,4 +1,4 @@
-import { blake2b, blakeHashBase64 } from './blake2b'
+import { blake2b, blakeHashHexSync } from './blake2b'
 import { Buffer } from 'buffer'
 
 describe('blake2b', () => {
@@ -13,7 +13,7 @@ describe('blake2b', () => {
   })
 
   it('should hash base64 string synchronously', () => {
-    const hash = blakeHashBase64('dGVzdA==')
+    const hash = blakeHashHexSync('74657374')
     expect(hash).toBe(
       '928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202'
     )
