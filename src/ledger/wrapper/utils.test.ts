25d8192611954d445b5718d3edc086e54bae7b3a
Dawid Sowa <dawid.sowa@rdx.works>
2023-07-10 16:41:43 +0200

    fix: make sure `getDataLength` return 2 chars

---
+++ src/ledger/wrapper/utils.test.ts
@@ -0,0 +1,17 @@
+import { getDataLength } from './utils'
+
+describe('utils', () => {
+  describe('getDataLength', () => {
+    it('should return two characters', () => {
+      expect(getDataLength('')).toEqual('00')
+      expect(getDataLength('6f7274210181010000002020002100')).toEqual('0f')
+      expect(
+        getDataLength(
+          '6f72742101810100000020200021006f7274210181010000002020002100',
+        ),
+      ).toEqual('1e')
+      expect(getDataLength('1111')).toEqual('02')
+      expect(getDataLength('12345678901234567890')).toEqual('0a')
+    })
+  })
+})
