5e2b79d43167e34f209d30b3cf64efe796a1c3c2
Dawid Sowa <dawid.sowa@rdx.works>
2024-05-08 22:02:08 +0200

    refactor: prettier

--- src/chrome/background/create-gateway-module.ts
+++ src/chrome/background/create-gateway-module.ts
@@ -1,18 +1,13 @@
-import {
-  RadixNetworkConfigById,
-} from '@radixdlt/babylon-gateway-api-sdk'
-import {
-  GatewayModule,
-} from '@radixdlt/radix-dapp-toolkit'
+import { RadixNetworkConfigById } from '@radixdlt/babylon-gateway-api-sdk'
+import { GatewayModule } from '@radixdlt/radix-dapp-toolkit'
 import { __VERSION__ } from 'version'
 
 export const createGatewayModule = (networkId: number) =>
-    GatewayModule({
-      clientConfig: {
-        basePath: RadixNetworkConfigById[networkId].gatewayUrl,
-        applicationName: 'Radix Connector Extension',
-        applicationVersion: __VERSION__,
-        applicationDappDefinitionAddress: '',
-      }
-    })
-  
+  GatewayModule({
+    clientConfig: {
+      basePath: RadixNetworkConfigById[networkId].gatewayUrl,
+      applicationName: 'Radix Connector Extension',
+      applicationVersion: __VERSION__,
+      applicationDappDefinitionAddress: '',
+    },
+  })
