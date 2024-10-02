de74a9f3bff253c5a9e9bd50c69767d503577352
Alex Stelea <alex.s@rdx.works>
2024-10-02 09:24:26 +0200

    code: use wildcard in url pattern match

--- src/chrome/helpers/get-tabs-by-origin.ts
+++ src/chrome/helpers/get-tabs-by-origin.ts
@@ -3,6 +3,6 @@ import { ResultAsync } from 'neverthrow'
 export const getTabsByOrigin = (origin: string) =>
   ResultAsync.fromPromise(
     // url pattern match requires a trailing slash
-    chrome.tabs.query({ url: `${origin}/` }),
+    chrome.tabs.query({ url: `${origin}/*` }),
     (err) => err as Error,
   )
