301f8b8b2ba110550f8d6617dd62a370b03f1420
Alex Stelea <alex.s@rdx.works>
2024-11-22 13:35:02 +0000

    code: remove leftovers

--- src/chrome/background/router/procedures/root.ts
+++ src/chrome/background/router/procedures/root.ts
@@ -1,8 +1,4 @@
-import { addContentScriptClientMiddleware } from '../middleware/addContentScriptClient'
-import { addOffscreenClientMiddleware } from '../middleware/addOffscreenClient'
 import { loggerMiddleware } from '../middleware/logger'
 import { trpc } from '../trpc'
 
 export const rootProcedure = trpc.procedure.use(loggerMiddleware)
-// .use(addOffscreenClientMiddleware)
-// .use(addContentScriptClientMiddleware)
